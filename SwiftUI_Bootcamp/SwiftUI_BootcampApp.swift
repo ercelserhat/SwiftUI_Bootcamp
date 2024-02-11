cfae51cee9cef86450e0ca60d4d712cc9bac9126
ercelserhat <ercelserhat@gmail.com>
2024-02-11 19:15:37 +0300

    Initial Commit

---
+++ SwiftUI_Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
