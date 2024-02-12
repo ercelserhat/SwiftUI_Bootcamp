7fc11ae43bc52c7601192f28fc0c9951e8b5d589
ercelserhat <ercelserhat@gmail.com>
2024-02-12 21:30:04 +0300

    Overlay

--- SwiftUI_Bootcamp/BackgroundAndOverlayBootcamp.swift
+++ SwiftUI_Bootcamp/BackgroundAndOverlayBootcamp.swift
@@ -9,19 +9,76 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootcamp: View {
     var body: some View {
-        Text("Hello, World!")
+//        Text("Hello, World!")
+//            .background(
+//               Color.red
+//               LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing)
+//                Circle()
+//                    .fill(LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 100, height: 100)
+//            )
+//            .background(
+//                Circle()
+//                    .fill(LinearGradient(colors: [.blue, .red], startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 120, height: 120)
+//            )
+        
+        Circle()
+            .fill(.pink)
+            .frame(width: 100, height: 100)
+            .overlay(
+                Text("1")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            )
             .background(
-//                Color.red
-//                LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing)
                 Circle()
-                    .fill(LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing))
+                    .fill(.purple)
+                    .frame(width: 110, height: 110)
+            )
+        
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .overlay(
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 50, height: 50)
+                , alignment: .topLeading
+            )
+            .background(
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 150, height: 150)
+                , alignment: .bottomTrailing
+            )
+        
+        Spacer()
+        
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundStyle(.white)
+            .background(
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [.purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    )
                     .frame(width: 100, height: 100)
+                    .shadow(color: .black.opacity(0.8), radius: 10, x: 0, y: 10)
+                    .overlay(
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                            )
+                            .shadow(color: .black.opacity(0.8), radius: 10, x: 5, y: 5)
+                        , alignment: .bottomTrailing
+                    )
             )
-            .background(
-                Circle()
-                    .fill(LinearGradient(colors: [.blue, .red], startPoint: .leading, endPoint: .trailing))
-                    .frame(width: 120, height: 120)
-            )
+        
+        Spacer()
     }
 }
 
