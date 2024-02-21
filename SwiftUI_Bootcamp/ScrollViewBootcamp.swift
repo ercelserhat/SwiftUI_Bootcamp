a707404f67db862193fa32344af1a61e90a9aa48
ercelserhat <ercelserhat@gmail.com>
2024-02-22 01:00:51 +0300

    ScrollView

---
+++ SwiftUI_Bootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 22.02.2024.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+//        ScrollView(.vertical, showsIndicators: false){
+//            VStack{
+//                ForEach(0..<50){ index in
+//                    Rectangle()
+//                        .fill(.blue)
+//                        .frame(height: 300)
+//                }
+//            }
+//        }
+        
+        ScrollView{
+            VStack{
+                ForEach(0..<10){index in
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            ForEach(0..<20){index in
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(.white)
+                                    .frame(width: 200, height: 150)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewBootcamp()
+}
