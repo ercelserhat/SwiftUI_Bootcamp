13fd769f7a0563678fdac740f4b03ecda2d73fda
ercelserhat <ercelserhat@gmail.com>
2024-02-22 01:02:15 +0300

    LazyVStack, LazyHStack

--- SwiftUI_Bootcamp/ScrollViewBootcamp.swift
+++ SwiftUI_Bootcamp/ScrollViewBootcamp.swift
@@ -20,10 +20,10 @@ struct ScrollViewBootcamp: View {
 //        }
         
         ScrollView{
-            VStack{
-                ForEach(0..<10){index in
+            LazyVStack{
+                ForEach(0..<100){index in
                     ScrollView(.horizontal, showsIndicators: false){
-                        HStack{
+                        LazyHStack{
                             ForEach(0..<20){index in
                                 RoundedRectangle(cornerRadius: 25.0)
                                     .fill(.white)
