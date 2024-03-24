b9233889966d8f68e731341717d0f3b25dc8700c
ercelserhat <ercelserhat@gmail.com>
2024-03-24 03:19:38 +0300

    TextField

---
+++ SwiftUI_Bootcamp/TextFieldBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  TextFieldBootcamp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 24.03.2024.
+//
+
+import SwiftUI
+
+struct TextFieldBootcamp: View {
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+        //            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .background(Color.gray.opacity(0.3).clipShape(RoundedRectangle(cornerRadius: 10)))
+                    .foregroundStyle(.red)
+                    .font(.headline)
+                
+                Button(action: {
+                    if textIsAppropriate(){
+                        saveText()
+                    }
+                }, label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ? Color.blue : Color.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                })
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self){data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextField Bootcamp")
+        }
+    }
+    
+    func textIsAppropriate() -> Bool{
+        //check text
+        if textFieldText.count >= 3{
+            return true
+        }
+        return false
+    }
+    
+    func saveText(){
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+#Preview {
+    TextFieldBootcamp()
+}
