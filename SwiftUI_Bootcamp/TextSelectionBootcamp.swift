619e14981c136bc0920a1b6e4e321beb1ca64ed0
ercelserhat <ercelserhat@gmail.com>
2024-03-31 00:35:55 +0300

    Text Selection

---
+++ SwiftUI_Bootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 31.03.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
