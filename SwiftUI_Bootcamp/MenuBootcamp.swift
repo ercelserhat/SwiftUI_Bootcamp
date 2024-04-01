415db8ee1251178f0283937e911c85596daa76eb
ercelserhat <ercelserhat@gmail.com>
2024-04-02 00:32:09 +0300

    Menu

---
+++ SwiftUI_Bootcamp/MenuBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  MenuBootcamp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 2.04.2024.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Clicke Me!") {
+            Button("One"){
+                
+            }
+            
+            Button("Two"){
+                
+            }
+            
+            Button("Three"){
+                
+            }
+            
+            Button("Four"){
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
