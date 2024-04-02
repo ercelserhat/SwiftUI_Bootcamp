c1892b0dd70580fc745b2471d0f561af147d0ebf
ercelserhat <ercelserhat@gmail.com>
2024-04-02 21:22:41 +0300

    Grid

---
+++ SwiftUI_Bootcamp/GridViewBootcamp.swift
@@ -0,0 +1,64 @@
+//
+//  GridViewBootcamp.swift
+//  SwiftUI_Bootcamp
+//
+//  Created by Serhat Erçel on 2.04.2024.
+//
+
+import SwiftUI
+
+struct GridViewBootcamp: View {
+    var body: some View {
+//        Grid(){
+//            GridRow{
+//                cell(int: 1)
+//                cell(int: 2)
+//                cell(int: 3)
+//            }
+//            Divider()
+//                .gridCellUnsizedAxes(.horizontal)
+//            GridRow{
+//                cell(int: 4)
+//                cell(int: 5)
+//            }
+//        }
+        
+        Grid(alignment: .leading, horizontalSpacing: 20, verticalSpacing: 20){
+            ForEach(0..<4){rowIndex in
+                GridRow(alignment: .bottom){
+                    ForEach(0..<4){columnIndex in
+                        let cellNumber = (rowIndex * 4) + (columnIndex + 1)
+                        if cellNumber == 7 {
+//                            EmptyView()
+//                            Color.green
+//                                .gridCellUnsizedAxes([.horizontal, .vertical])
+                            Color.clear
+                                .gridCellUnsizedAxes([.horizontal, .vertical])
+                            
+                        } else{
+                            cell(int: cellNumber)
+                                .gridCellColumns(cellNumber == 6 ? 2 : 1)
+                                .gridCellAnchor(.trailing)
+                                .gridColumnAlignment(cellNumber == 1 ? .trailing :
+                                                        cellNumber == 4 ? .leading : .center
+                                )
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func cell(int: Int) -> some View{
+        Text("\(int)")
+            .frame(height: int == 10 ? 20 : nil)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            
+    }
+}
+
+#Preview {
+    GridViewBootcamp()
+}
